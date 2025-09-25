52a9eefc57b6d7e1d5444460be779242382a7da5
kaneda05 <kanemasa1541km@gmail.com>
2025-09-26 01:03:27 +0900

    solve

---
+++ D/Java/210.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        System.out.println(12*p);
+    }
+}
